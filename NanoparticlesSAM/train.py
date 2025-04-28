4cba4908faa4bd4d638616c159293673f9b2a2e4
biolographer <a.debon7@gmail.com>
2025-04-28 16:36:46 +0200

    validation and logging

--- NanoparticlesSAM/train.py
+++ NanoparticlesSAM/train.py
@@ -17,29 +17,43 @@ from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
 
-IMAGE_FOLDER = '/content/drive/MyDrive/shield_data/training data'
-EPOCHS = 100000
+
+TRAIN_IMAGE_FOLDER = '/content/drive/MyDrive/shield_data/training data'
+TEST_IMAGE_FOLDER = '/content/drive/MyDrive/shield_data/testing data'
+EPOCHS = 2000
 
 data = CircleMaskDataset(
-    image_dir=IMAGE_FOLDER,
+    image_dir=TRAIN_IMAGE_FOLDER,
     metadata_fn=get_circle_metadata,
     crop_banner=True,
     convert_to_tensor=False,
 )
 
+test_data = CircleMaskDataset(
+    image_dir=TEST_IMAGE_FOLDER,
+    metadata_fn=get_circle_metadata,
+    crop_banner=True,
+    convert_to_tensor=False,
+)
 
-def read_batch(data): # read random image and its annotaion from  the dataset (LabPics)
+print(f'loaded training data, {len(data)} images')
+
+
+
+def read_batch(data, index=None): # read random image and its annotaion from  the dataset (LabPics)
     #  select image
+    if index == None:
+        ent  = data[np.random.randint(len(data))] # choose random entry
+    else:
+        ent = data[index]
+    Img = ent[0]  # read image
+    particle_annotation = ent[1] # read annotation
 
-     ent  = data[np.random.randint(len(data))] # choose random entry
-     Img = ent[0]  # read image
-     particle_annotation = ent[1] # read annotation
 
-
-     inds = np.unique(particle_annotation)[1:] # get the value for all masks
-     points= []
-     masks = []
-     for ind in inds:
+    inds = np.unique(particle_annotation)[1:] # get the value for all masks
+    points= []
+    masks = []
+    for ind in inds:
           mask = (particle_annotation == ind).astype(np.uint8) # make binary mask corresponding to index ind
           masks.append(mask)
           coords = np.argwhere(mask > 0) # get all coordinates in mask
@@ -64,7 +78,53 @@ def read_batch(data): # read random image and its annotaion from  the dataset (L
           yx = np.array(filtered_coords[np.random.randint(len(filtered_coords))]) 
           points.append([[yx[1], yx[0]]])
           
-     return Img,np.array(masks),np.array(points), np.ones([len(masks),1])
+    return Img,np.array(masks),np.array(points), np.ones([len(masks),1])
+
+def validation_loop(val_data):
+     val_loss = 0
+     val_iou = 0
+     with torch.no_grad(): 
+        for i in range(len(val_data)):
+            image, mask, input_point, input_label = read_batch(val_data, index=i)
+            if mask.shape[0] == 0:
+                continue  # Ignore empty batches
+            predictor.set_image(image)  # Apply SAM image encoder to the image
+            mask_input, unnorm_coords, labels, unnorm_box = predictor._prep_prompts(input_point, input_label, box=None, mask_logits=None, normalize_coords=True)
+            sparse_embeddings, dense_embeddings = predictor.model.sam_prompt_encoder(points=(unnorm_coords, labels), boxes=None, masks=None)
+
+            # Mask decoder
+            batched_mode = unnorm_coords.shape[0] > 1  # Multi-object prediction
+            high_res_features = [feat_level[-1].unsqueeze(0) for feat_level in predictor._features["high_res_feats"]]
+            low_res_masks, prd_scores, _, _ = predictor.model.sam_mask_decoder(
+                image_embeddings=predictor._features["image_embed"][-1].unsqueeze(0),
+                image_pe=predictor.model.sam_prompt_encoder.get_dense_pe(),
+                sparse_prompt_embeddings=sparse_embeddings,
+                dense_prompt_embeddings=dense_embeddings,
+                multimask_output=True,
+                repeat_image=batched_mode,
+                high_res_features=high_res_features,
+            )
+            prd_masks = predictor._transforms.postprocess_masks(low_res_masks, predictor._orig_hw[-1])  # Upscale the masks to the original image resolution
+
+            # Segmentation Loss calculation (cross-entropy loss)
+            gt_mask = torch.tensor(mask.astype(np.float32)).cuda()
+            prd_mask = torch.sigmoid(prd_masks[:, 0])  # Turn logit map to probability map
+            seg_loss = (-gt_mask * torch.log(prd_mask + 0.00001) - (1 - gt_mask) * torch.log((1 - prd_mask) + 0.00001)).mean()
+
+            # Score loss calculation (Intersection over Union - IOU)
+            inter = (gt_mask * (prd_mask > 0.5)).sum(1).sum(1)
+            iou = inter / (gt_mask.sum(1).sum(1) + (prd_mask > 0.5).sum(1).sum(1) - inter)
+            score_loss = torch.abs(prd_scores[:, 0] - iou).mean()
+
+            val_loss += seg_loss + score_loss * 0.05  # Add to validation loss
+            val_iou += np.mean(iou.cpu().detach().numpy())  # Add to IOU
+
+        # Average validation results
+        val_loss /= len(val_data)
+        val_iou /= len(val_data)
+        print(f"Validation - Step {itr}, Loss = {val_loss}, IOU = {val_iou}")
+        return (val_loss, val_iou)     
+
 
 # set up device
 if torch.cuda.is_available():
@@ -96,6 +156,9 @@ optimizer=torch.optim.AdamW(params=predictor.model.parameters(),lr=1e-5,weight_d
 scaler = torch.amp.GradScaler('cuda')
 
 # Training loop
+
+outfile = open('/content/training_log.csv','w')
+outfile.write('step,loss,iou,stage')
 
 for itr in tqdm(range(EPOCHS)):
     with torch.amp.autocast(device_type='cuda'): # cast to mix precision
@@ -141,4 +204,14 @@ for itr in tqdm(range(EPOCHS)):
 
             if itr==0: mean_iou=0
             mean_iou = mean_iou * 0.99 + 0.01 * np.mean(iou.cpu().detach().numpy())
-            print("step)",itr, "Accuracy(IOU)=",mean_iou)
+            print("step",itr, "Accuracy(IOU)=",mean_iou)
+            outfile.write(f'{itr},{loss},{mean_iou},train')
+
+
+            if itr % 20==0 and itr > 1:
+                print('____________________________________\n\tvalidating\n____________________________________\n')
+                val_loss, val_iou = validation_loop(test_data)
+                outfile.write(f'{itr},{val_loss},{val_iou},test')
+                print('____________________________________\n')
+
+outfile.close()
